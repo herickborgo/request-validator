3cbfd2e09c462367a53bdeef9b4efeaeb4251488
Herick Borgo <hericoborgo1@gmail.com>
2021-03-13 18:00:28 -0300

    Implemented Request Validator Tests

--- src/request_validator.ts
+++ src/request_validator.ts
@@ -5,8 +5,8 @@ const fs = require('fs');
 
 abstract class RequestValidator extends Annotation {
 
-    private defaultRules: Object = {
-        'required': (v) => !!v,
+    private defaultRules: any = {
+        'required': (v: any) => !!v,
     };
 
     protected req: any = null;
@@ -19,17 +19,24 @@ abstract class RequestValidator extends Annotation {
     abstract rules(): Object;
 
     public validate(): void {
-        const rules = this.rules();
+        const rules: any = this.rules();
         const data = Object.keys(rules).map((key) => {
-            const result = {};
-            result[key] = {};
-            if (typeof rules[key] === 'string') {
-                const rule = rules[key];
-                result[key][rule] = this.defaultRules[rule](this.req[key]);
+            const rule: any = rules[key];
+            const result: any = {
+                field: key,
+                errors: [],
+            };
+            if (typeof rule === 'string') {
+                result.errors.push({
+                    rule,
+                    result: this.defaultRules[rule](this.req[key]),
+                });
             }
-            if (typeof rules[key] === 'function') {
-                const rule = rules[key];
-                result[key][rule] = rules[key](this.req[key]);
+            if (typeof rule === 'function') {
+                result.errors.push({
+                    rule,
+                    result: rules[key](this.req[key]),
+                });
             }
             return result;
         });
@@ -38,19 +45,28 @@ abstract class RequestValidator extends Annotation {
 
     protected messages(validations: Array<object>) {
         const errors: Array<object> = []
-        validations.forEach((validation) => {
-            const fields = Object.keys(validation);
-            fields.forEach((field) => {
-                const rules = Object.keys(validation[field]);
+        validations.forEach((validation: any) => {
+            const field: string = validation.field;
 
-                rules.forEach((rule) => {
-                    if (!validation[field][rule]) {
-                        const message = this.getMessage(rule);
-                        const error = {};
-                        error[field] = message.replace(':attribute', field);
-                        errors.push(error);
-                    }
-                })
+            validation.errors.forEach((error: any) => {
+                let message = this.getMessage(error.rule);
+                const result: any = {};
+                result[field] = {}
+
+                if (!message) {
+                    message = 'The field :attribute has an error';
+                }
+
+                if (typeof error.result === 'string') {
+                    message = error.result;
+                    error.rule = error.rule.name;
+                    error.result = false;
+                }
+
+                if (!error.result) {
+                    result[field][error.rule] = message.replace(':attribute', field);
+                }
+                errors.push(result);
             })
         });
 
@@ -59,7 +75,7 @@ abstract class RequestValidator extends Annotation {
         }
     }
 
-    private getMessage(key: string, language: string = 'en-US') {
+    private getMessage(key: string, language: string = 'en-US'): string {
         const fileMessage = `${__dirname}/messages/${language}.json`;
         if (fs.existsSync(fileMessage)) {
             const messages = require(fileMessage);
